bce3f4e005c455ba2b8a30f406f77168a567a4d3
Shivang <shivangsudhirsonker@gmail.com>
2022-01-29 00:21:44 +0530

    Added title to GameDev Web Page

--- src/pages/GameDev.tsx
+++ src/pages/GameDev.tsx
@@ -1,8 +1,13 @@
 import { motion } from "framer-motion";
 import { AnimatedPage } from "../Components/Content";
+import TopHeading from "../Components/TopHeading";
 
 const GameDev = () => {
-  return <AnimatedPage html={<div>This is game dev</div>} />;
+  return <AnimatedPage html={
+  <div>
+    <TopHeading text="Game Development"></TopHeading>
+  </div>
+} />;
 };
 
 export default GameDev;
